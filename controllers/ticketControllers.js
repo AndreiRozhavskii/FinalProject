ebe4da64229f5b44a87e131040a574f3dea51969
Andrei <150835122+AndreiRozhavskii@users.noreply.github.com>
2024-07-11 22:30:55 +0300

    Update ticketControllers.js

--- controllers/ticketControllers.js
+++ controllers/ticketControllers.js
@@ -1,147 +1,83 @@
-import { getAdminTickets, getUserTickets,getTicketByID, createTicket, resolveTicket, getResolvedTicketsByStatus,addMessage, getTicketMessages, getCurrentTicket } from "../models/ticketModels.js";
-import { getUserByRole_1 } from "../models/authModels.js";
+import { db } from '../config/db.js'
 
-//get all tickets
 
-// export const _getAllTickets = async (req, res) => {
-//     try {
-//         const tasks = await getAllTickets();
-//         res.status(200).json(tasks);
-//     } catch (error) {
-//         res.status(500).json({ error: error.message });
-//     }
+// export const getAllTickets = async() =>{
+//     return db('tickets').select('*');
 // };
 
-export const _getTickets = async(req,res)=>{
+export const getUserTickets = async(user_id) =>{
     
-    if (req.userrole==2){
-        try {
-            const user_id = req.userid;
-            const tasks = await getUserTickets(user_id);
-            res.status(200).json(tasks);
-        } 
-        catch (error) {
-            res.status(error).json({ error: error.message });
-        }
-    }
-    else {
-        try {
-            const tasks = await getAdminTickets();
-            res.status(200).json(tasks);
-        } 
-        catch (error) {
-            res.status(error).json({ error: error.message });
-        }
-    }
+    
+    return  db('tickets').where({user_id}).select('ticket_id', 'title', 'description', 'status', 'priority', 'created_at');
+};
+
+export const getAdminTickets=async()=>{
+    
+    return db('tickets').where('status', 'not solved').select('*');
 }
 
 
-
-
-
-//create ticket 
-
-export const _createTicket = async (req, res) => {
-    const { title, description, priority} = req.body;
-    const user_id=req.userid;
-    try {
+export const createTicket = async(title, description, priority,user_id) =>{
     
-        const newTask = await createTicket(title, description, priority,user_id);
-        console.log('new task',newTask);
-        res.status(201).json(newTask);
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: error.message });
-    }
-};
-
-//resolve ticket 
-
-export const _resolveTicket = async (req, res) => {
-    const { ticket_id } = req.params;
-    const user_id = req.userid
-    
-    try {
-        await resolveTicket(ticket_id,user_id);
-        res.status(200).json({ message: 'Ticket resolved successfully' });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'An error occurred while resolving ticket' });
-    }
-};
-
-//get resolved ticket
-
-export const _getResolvedTicketsByStatus = async (req, res) => {
-    const { status } = req.params;
-    
-    try {
-        const resolvedTickets = await getResolvedTicketsByStatus(status);
-        res.status(200).json(resolvedTickets);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'An error occurred while fetching resolved tickets' });
-    }
-};
-
-// MESSAGES
-
-export const _addMessage = async (req, res) => {
-    const { ticket_id } = req.params;
-    const { text } = req.body;
-    const from_user_id = req.userid;
-
-    try {
-        
-        const ticket = await getTicketByID(ticket_id);
-        const to_user_id = ticket.user_id;
-
-        if (from_user_id === to_user_id) {
-            
-            const usersWithRole_1 = await getUserByRole_1();
-
-            for (const user of usersWithRole_1) {
-                await addMessage(ticket_id, from_user_id, user.user_id, text);
-            }
-            
-        } else {
-            
-            await addMessage(ticket_id, from_user_id, to_user_id, text);
-        }
-
-        _getTicketMessages(req, res);
-
-    } catch (error) {
-        res.status(500).json({ message: 'An error occurred while adding the message.', error });
-    }
-};
-
-// Working controller
-export const _getTicketMessages = async (req, res) => {
-    const { ticket_id } = req.params;
-
-    try {
-        const messages = await getTicketMessages(ticket_id);
-        res.status(200).json(messages);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'An error occurred while fetching ticket messages' });
-    }
+    return db('tickets').insert({ title, description, priority,user_id},['title', 'description', 'priority']);
 };
 
 
+export const resolveTicket = async(ticket_id,user_id)=>{
+    return db('tickets').where({ticket_id}).update({status:'solved',resolved_at:new Date(),resolved_by:user_id})
+}
+
+//knowledgebase
+
+export const getResolvedTicketsByStatus = async () => {
+    return db('tickets')
+        .select(
+            'tickets.ticket_id',
+            'tickets.title',
+            'tickets.description',
+            'tickets.status',
+            'users.username as created_by_username',
+            'tickets.created_at',
+            'resolver.username as resolved_by_username',
+            'tickets.resolved_at'
+        )
+        .join('users', 'tickets.user_id', 'users.user_id')
+        .leftJoin('users as resolver', 'tickets.resolved_by', 'resolver.user_id')
+        .where('tickets.status', 'solved');
+};
 
 
+  //CHAT
+  export const getTicketByID = async (ticket_id) => {
+    return db('tickets').where({ticket_id}).first()
+  }
 
+  export const addMessage = async (ticket_id, from_user_id, to_user_id, text) => {
+    return db('messages').insert({
+        ticket_id,
+        from_user_id,
+        to_user_id,
+        text,
+        created_at: new Date(),
+    });
+};
 
+export const getTicketMessages = async (ticket_id) => {
+    return db('messages').select(
+          'messages.message_id',
+          'messages.ticket_id',
+          'users_from.username as from_username',
+          'users_to.username as to_username',
+          'messages.text',
+          'messages.created_at'
+        )
+        .join('users as users_from', 'messages.from_user_id', 'users_from.user_id')
+        .join('users as users_to', 'messages.to_user_id', 'users_to.user_id')
+        .where({ ticket_id })
+        .orderBy('created_at', 'asc');
+};
 
-export const _getCurrentTicket = async (req,res) =>{
-    const {ticket_id} = req.params;
-    try{
-        const ticket = await getCurrentTicket(ticket_id);
-        res.status(200).json(ticket);
-    }  catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'An error occurred while fetching ticket messages' });
-    }
-};   
+// GET PARTICUALLY TICKET
+export const getCurrentTicket = async(ticket_id)=>{
+    return  db('tickets').where({ticket_id}).select('ticket_id', 'title', 'description', 'status');
+};
