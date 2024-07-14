202e3452134dbd5583c760c07cdd89ddd57e350c
Andrei <150835122+AndreiRozhavskii@users.noreply.github.com>
2024-07-14 17:41:55 +0300

    Update Navbar.jsx
    hide users+registration from users who is not admin

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,9 @@
-import React, { useContext, useState} from "react";
-import { Link,useNavigate } from "react-router-dom";
+import React, { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../App";
 
 const Navbar = () => {
-  const { token, setToken } = useContext(AuthContext);
-  
+  const { token, setToken, userRole } = useContext(AuthContext);
   const navigate = useNavigate();
 
   const handleLogout = () => {
@@ -34,13 +33,17 @@ const Navbar = () => {
         <div>Knowledge Base</div>
       </Link>
 
-      <Link to="/dashboard/register" style={{ textDecoration: 'none', color: 'black', border: '1px solid black', padding: '10px', borderRadius: '5px', marginBottom: '10px', marginLeft: '10px' }}>
-        <div>User Registration</div>
-      </Link>
+      {userRole === 1 && (
+        <>
+          <Link to="/dashboard/register" style={{ textDecoration: 'none', color: 'black', border: '1px solid black', padding: '10px', borderRadius: '5px', marginBottom: '10px', marginLeft: '10px' }}>
+            <div>User Registration</div>
+          </Link>
 
-      <Link to="/dashboard/users" style={{ textDecoration: 'none', color: 'black', border: '1px solid black', padding: '10px', borderRadius: '5px', marginBottom: '10px', marginLeft: '10px' }}>
-        <div>Users</div>
-      </Link>
+          <Link to="/dashboard/users" style={{ textDecoration: 'none', color: 'black', border: '1px solid black', padding: '10px', borderRadius: '5px', marginBottom: '10px', marginLeft: '10px' }}>
+            <div>Users</div>
+          </Link>
+        </>
+      )}
 
       {token ? (
         <button onClick={handleLogout} style={{ border: '1px solid black', padding: '10px', borderRadius: '5px', backgroundColor: '#007bff', color: '#fff', cursor: 'pointer', marginLeft: '10px', marginBottom: '10px' }}>
