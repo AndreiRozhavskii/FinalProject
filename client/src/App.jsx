89d4ea9b6a8bd4d4b8153bfa74a64cc2aa9fc20d
Andrei <150835122+AndreiRozhavskii@users.noreply.github.com>
2024-07-14 17:42:43 +0300

    Update App.jsx
    hide users+reg from who is not admin

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
-import { useState, createContext } from "react";
-import { Routes, Route, Navigate} from "react-router-dom";
-import axios from "axios";
+import { useState, createContext, useContext } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Auth from "./auth/Auth";
@@ -12,19 +11,16 @@ import "./App.css";
 import KnowledgeBase from "./components/KnowledgeBase";
 import TicketDetail from "./components/TicketDetail";
 
-
 export const BASE_URL = import.meta.env.VITE_APP_BASE_URL;
 export const AuthContext = createContext();
 
 function App() {
-  
   const [token, setToken] = useState();
-  const [userRole,setUserRole] = useState();
-  const [userID, setUserID]=useState();
-
+  const [userRole, setUserRole] = useState();
+  const [userID, setUserID] = useState();
 
   return (
-    <AuthContext.Provider value={{ token, setToken,setUserRole, userRole, setUserID, userID }}>
+    <AuthContext.Provider value={{ token, setToken, setUserRole, userRole, setUserID, userID }}>
       <Routes>
         <Route path="/login" element={<Login page="Login" />} />
         <Route
@@ -33,22 +29,19 @@ function App() {
             <Auth>
               <Navbar />
               <Routes>
-              <Route path="/" element={<Home />} />
+                <Route path="/" element={<Home />} />
                 <Route path="/dashboard" element={<Home />} />
                 <Route path="/dashboard/knowledgebase" element={<KnowledgeBase />} />
                 <Route path="/dashboard/:ticket_id" element={<TicketDetail />} />
-                
-                  <>
-                  
-                    {userRole === 1 && (
+                <>
+                  {userRole === 1 && (
+                    <>
                       <Route path="/dashboard/register" element={<Register />} />
-                    )}
-                    {userRole === 1 && (
                       <Route path="/dashboard/users" element={<Users />} />
-                    )}
-                    <Route path="/dashboard/createticket" element={<CreateTicket />} />
-                  </>
-              
+                    </>
+                  )}
+                  <Route path="/dashboard/createticket" element={<CreateTicket />} />
+                </>
               </Routes>
             </Auth>
           }
@@ -58,40 +51,4 @@ function App() {
   );
 }
 
-
-
 export default App;
-// function App() {
-//   const [token, setToken] = useState();
-
-//   return (
-//     <AuthContext.Provider value={{ token, setToken }}>
-      
-//       <Routes>
-        
-//       <Route
-//         path="/"
-//         element={
-//           token ? (
-//             <Auth>
-//               <Navbar/>
-//               <Home />
-//             </Auth>
-//           ) : (
-//             <Navigate to="/login" />
-//           )
-//         }
-//       />
-//         <Route path='/login' element={<Login page="Login"/>}/> 
-//         <Route path='/dashboard/knowledgebase' element={<Auth><Navbar/><KnowledgeBase /></Auth>}/>
-//         <Route path='/dashboard/register' element={<Auth><Navbar/><Register /></Auth>}/>
-//         <Route path='/dashboard/users' element={<Auth><Navbar/><Users /></Auth>}/>
-//         <Route path='/dashboard/createticket' element={<Auth><Navbar/><CreateTicket /></Auth>}/>
-//         <Route path='/dashboard' element={<Auth><Navbar/><Home /></Auth>}/>
-//       </Routes>
-      
-//     </AuthContext.Provider>
-//   );
-// }
-
-// export default App;
