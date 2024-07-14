c5ce3eec6eea021f5b57b32136af9a7811076e94
Andrei <150835122+AndreiRozhavskii@users.noreply.github.com>
2024-07-14 20:06:56 +0300

    Update authModels.js
    add choosing role

--- models/authModels.js
+++ models/authModels.js
@@ -1,7 +1,7 @@
 import {db} from '../config/db.js';
 
-export const register =(username,email,password)=>{
-    return db('users').insert({username,email,password},['user_id','email']);
+export const register =(username,email,password,role_id)=>{
+    return db('users').insert({username,email,password,role_id},['user_id','email']);
 };
 
 export const login =(email)=>{
@@ -10,7 +10,7 @@ export const login =(email)=>{
 };
 
 export const getAllUsers=()=>{
-    return db('users','roles').select('users.user_id','users.email','users.password','users.username','roles.role_name')
+    return db('users').select('users.user_id','users.email','users.password','users.username','roles.role_name')
         .from('users')
         .join('roles', 'roles.role_id', 'users.role_id');
 }
@@ -22,4 +22,8 @@ export const deleteUser=(user_id)=>{
 export const getUserByRole_1 = () => {
     return db('users').where('role_id',1).select('*');
 };
+
+export const getRoleName = (role_name) => {
+    return db('roles').select('role_id').where({role_name})
+}
 
